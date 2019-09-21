9922b11a137d9c94cd5074cec116188f62a05e0a
evans-github <evan.li88@yahoo.com>
2019-09-21 14:29:47 -0700

    Commit

--- queries.js
+++ queries.js
@@ -7,7 +7,7 @@ const cassandra = require('cassandra-driver');
 const client = new cassandra.Client({ contactPoints: ['127.0.0.1'], localDataCenter: 'datacenter1' });
 
 const getStreaming = (request, response) => {
-    console.log('STREAMING BRO');
+    console.log('STREAMING BRO TEST 1');
     response.status(200).json("IM STREAMING");
 };
 
