6efed5344e1f42c9437aefe79c7ff78de40d4e0a
evans-github <evan.li88@yahoo.com>
2019-09-21 13:58:31 -0700

    Commit

--- queries.js
+++ queries.js
@@ -1,10 +1,21 @@
 
 const cassandra = require('cassandra-driver');
+const WebSocket = require('ws');
 
+const wss = new WebSocket.Server({ port: 8080 });
 
 const client = new cassandra.Client({ contactPoints: ['127.0.0.1'], localDataCenter: 'datacenter1' });
 
 const getFollowing = (request, response) => {
+
+  wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
+    ws.send('I got your message: ' + message);
+  });
+
+
+});
 
 
   const id = request.params.id;
